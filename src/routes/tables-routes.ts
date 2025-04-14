522da8907752c79e9a858c67cb11675fa4f92db8
A1n2F <alissonfferreira22@gmail.com>
2025-04-13 21:04:26 -0300

    Listando as mesas

---
+++ src/routes/tables-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import { TablesController } from "@/controller/tables-controller"
+
+const tablesRoutes = Router()
+const tablesController = new TablesController()
+    
+tablesRoutes.get("/", tablesController.index)
+
+export { tablesRoutes }
